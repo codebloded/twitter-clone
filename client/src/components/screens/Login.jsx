2811525ebcadbed6f52c408d5c9239a14771a95f
Rohan <rohanrajput16.phy@gmail.com>
2020-12-22 04:04:38 +0530

    making login and landing page ;)

---
+++ client/src/components/screens/Login.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+import { Button, Container, Paper, TextField, Typography } from '@material-ui/core';
+import TwitterIcon from "@material-ui/icons/Twitter"
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+  
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        color: 'white'
+    },
+    btn: {
+        margin: '4px'
+    },
+    paper: {
+        padding: theme.spacing(6),
+        textAlign: 'center',
+        width: '100vw',
+
+    },
+    txt: {
+        fontWeight: 'bold',
+
+    },
+    mainDiv: {
+        paddingTop: '100px',
+        paddingBottom: '100px',
+        backgroundColor: "rgb(26, 27, 31)",
+        color: 'white',
+        textAlign: 'center'
+    },
+    img: {
+        height: "90vh",
+        width: "fixContent"
+    },
+    box: {
+        display: 'flex',
+        justifyContent: 'center'
+    }
+}));
+
+export default function Login() {
+    const classes = useStyles();
+    const rootRef = React.useRef(null);
+    
+    return (
+        <div>
+            <Modal
+        disablePortal
+        disableEnforceFocus
+        disableAutoFocus
+        open
+        aria-labelledby="server-modal-title"
+        aria-describedby="server-modal-description"
+        className={classes.modal}
+        container={() => rootRef.current}
+      >
+              
+                    <Paper >
+                        <Container className={classes.mainDiv}>
+                            <TwitterIcon fontSize="large" color="primary" />
+                            <Typography variant="h4" component="h1" className={classes.txt}>
+                                See what's happening in the world right now
+                            </Typography>
+                            <form>
+                                <Container className={classes.form}>
+                                    <TextField id='standard-basic' label='username' />
+                                    <TextField id='standard-basic' label='password' />
+                                </Container>
+                            </form>
+                            <Button className={classes.btn} variant="contained" color="primary">Login</Button>
+                            <p>Have an account?</p>
+                        </Container>
+                    </Paper>
+            </Modal>
+        </div>
+    );
+}
---
+++ client/src/components/screens/Start.jsx
@@ -0,0 +1,80 @@
+import React from "react"
+import { Container, makeStyles, Button, Typography } from '@material-ui/core';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import SmsIcon from '@material-ui/icons/Sms';
+import GroupIcon from '@material-ui/icons/Group';
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        display: 'flex',
+
+    },
+    txt: {
+        fontWeight: 'bolder',
+        color: 'black'
+    },
+    btn: {
+        display: 'flex',
+        flexDirection: "column",
+       
+    },
+    button:{
+        margin: "3px",
+        borderRadius: "18px",
+         backgroundColor: 'black',
+        color: "white",
+        width: "-webkit-fill-available",
+    },
+    link:{
+        textDecoration:'none'
+    }
+
+
+}))
+
+const Start = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.box}>
+
+            <div className='App-header'>
+                <Container maxWidth="lg">
+                    <div>
+                        <div className={classes.box}>
+                            <SearchRoundedIcon fontSize="large" />
+                            <h5>Follow your interests.</h5>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div>
+                        <div className={classes.box}>
+                            <GroupIcon fontSize="large" />
+                            <h5>Hear what people are talking about.</h5>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div>
+                        <div className={classes.box}>
+                            <SmsIcon fontSize="large" />
+                            <h5>Join the conversation.</h5>
+                        </div>
+                    </div>
+                </Container>
+                <Container fixed >
+                    <Typography variant="h4" component="h2" className={classes.txt}>
+                        See what's happening in the world right now
+       </Typography>
+                    <br></br>
+                    <p className={classes.txt}>Join twitter today</p>
+                    <div className={classes.btn}>
+                        <Button variant="contained" color='primary' className={classes.button}>Sign up</Button>
+                       <Link to="/login" className={classes.link}> <Button variant="contained" color="primary"className={classes.button} >Login</Button></Link>
+                    </div>
+                </Container>
+            </div>
+        </div>
+
+    )
+}
+export default Start;
