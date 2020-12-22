52d414ab6a9151cfc4af39355537af76b9b9af77
Rohan <rohanrajput16.phy@gmail.com>
2020-12-22 22:21:44 +0530

    signup route done!

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:'mongodb+srv://rohan:S08NxiIFafsMm3wG@twitter-clone-radix.qdx5v.mongodb.net/test?authSource=admin&replicaSet=atlas-11ij8f-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    MONGOURI:'mongodb+srv://rohan:S08NxiIFafsMm3wG@twitter-clone-radix.qdx5v.mongodb.net/test?authSource=admin&replicaSet=atlas-11ij8f-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    JWT_SECRET:'kjfkdjssfnjdhfjndsshfh'
 }
--- routes/authentication.js
+++ routes/authentication.js
@@ -4,8 +4,8 @@ const { findOne } = require('../models/user');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const router = express.Router()
-
-// ==================LOGIN ROUTE=============
+const {JWT_SECRET} = require('../config/dev')
+// ==================SIGNUP ROUTE=============
 router.get('/t', (req, res)=>{
     res.send({message:"Hemllo"})
 });
@@ -36,6 +36,36 @@ router.post('/signup', async(req,res)=>{
         res.json({err:error});
     }
 });
+
+// =============================LOGIN ROUTE=======================
+router.post('/login',async(req, res)=>{
+    req.header('Content-Type', 'application/json');
+    const{email, password} = req.body;
+    if(!email || !password){
+        return res.status(402).json({error:"Please Fill all credentials"});
+    }
+    try {
+        const savedUser = await User.findOne({email:email});
+        if(!savedUser){
+            return res.status(404).json({error:"Invalid Username or password"});
+        }
+        const matchPassword = await bcrypt.compare(password, savedUser.password);
+        if(!matchPassword){
+            res.status(402).json({error:'Invalid passowrd or email address'});
+        }else{
+            //JSON -Token-
+            const token =  JWT.sign({_id:savedUser._id},JWT_SECRET);
+            const {_id, name, email}  = savedUser;
+            res.status(200).json({token,user:{_id, name, email}});
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+
+})
 
 
 
